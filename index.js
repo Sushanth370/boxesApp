9755b0a3a687defbd8e9203a5a11e1ffd4ff298d
Sushanth <sushanthreddy370@gmail.com>
2024-07-18 18:45:22 +0530

    demo

---
+++ index.js
@@ -0,0 +1,16 @@
+const Box = (props) => {
+  //  Write your code here.
+  <div></div>;
+};
+
+const element = (
+  //  Write your code here.
+  <div>
+    <h1>dkk</h1>
+    <Box />
+    <Box />
+    <Box />
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
